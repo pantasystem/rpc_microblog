16c8efd41cdca6c893426e4e80abdf21f226eab5
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-14 12:34:18 +0900

    feat: 認証用のミドルウェアを追加

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.3.1 // indirect
--- pkg/service/handler/handler.go
+++ pkg/service/handler/handler.go
@@ -2,17 +2,29 @@ package service
 
 import (
 	// "com.github/pantasystem/rpc-chat/pkg/impl"
+	"context"
+	"fmt"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"systems.panta/rpc-microblog/pkg/module"
 )
 
-func Setup() *grpc.Server {
+type Key int
+
+const (
+	AccountId Key = iota
+)
+
+func Setup(m module.Module) *grpc.Server {
 	s := grpc.NewServer(
-	// grpc.UnaryInterceptor(
-	// 	grpc_middleware.ChainUnaryServer(
-	// 		NewAuthInterceptor(core),
-	// 	),
-	// ),
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(
+				NewAuthInterceptor(m),
+			),
+		),
 	)
 
 	// accountService := AccountService{
@@ -33,46 +45,46 @@ func Setup() *grpc.Server {
 	return s
 }
 
-// func NewAuthInterceptor(c *impl.Core) func(ctx context.Context,
-// 	req interface{},
-// 	info *grpc.UnaryServerInfo,
-// 	handler grpc.UnaryHandler,
-// ) (interface{}, error) {
-// 	return func(ctx context.Context,
-// 		req interface{},
-// 		info *grpc.UnaryServerInfo,
-// 		handler grpc.UnaryHandler,
-// 	) (interface{}, error) {
-// 		fmt.Printf("info.FullMethod: %v\n", info.FullMethod)
-// 		// 認証をスキップするパス
-// 		if info.FullMethod == "/AccountService/CreateAccount" {
-// 			return handler(ctx, req)
-// 		} else {
-// 			// 認証がOKならContextを返す
-// 			userIdSetCtx, err := authorize(c, ctx)
-// 			if err != nil {
-// 				if info.FullMethod == "/AccountService/FindMe" {
-// 					return handler(ctx, req)
-// 				}
-// 				return nil, err
-// 			}
-// 			return handler(userIdSetCtx, req)
-// 		}
-// 	}
+func NewAuthInterceptor(c module.Module) func(ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (interface{}, error) {
+	return func(ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		fmt.Printf("info.FullMethod: %v\n", info.FullMethod)
+		// 認証をスキップするパス
+		if info.FullMethod == "/AccountService/CreateAccount" {
+			return handler(ctx, req)
+		} else {
+			// 認証がOKならContextを返す
+			userIdSetCtx, err := authorize(c, ctx)
+			if err != nil {
+				if info.FullMethod == "/AccountService/FindMe" {
+					return handler(ctx, req)
+				}
+				return nil, err
+			}
+			return handler(userIdSetCtx, req)
+		}
+	}
 
-// }
+}
 
-// func authorize(core *impl.Core, ctx context.Context) (context.Context, error) {
-// 	// ヘッダーのトークンからユーザIDを取得
-// 	token, err := grpc_auth.AuthFromMD(ctx, "Bearer")
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	// jwtのトークンを検証してuserIdを取得（処理の記載は割愛）
-// 	userId, err := core.NewAccountRepository().FindByToken(ctx, token)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+func authorize(module module.Module, ctx context.Context) (context.Context, error) {
+	// ヘッダーのトークンからユーザIDを取得
+	token, err := grpc_auth.AuthFromMD(ctx, "Bearer")
+	if err != nil {
+		return nil, err
+	}
+	// jwtのトークンを検証してuserIdを取得（処理の記載は割愛）
+	userId, err := module.RepositoryModule().AccountRepository().FindByToken(ctx, token)
+	if err != nil {
+		return nil, err
+	}
 
-// 	return context.WithValue(ctx, "accountId", userId.Id.String()), nil
-// }
+	return context.WithValue(ctx, AccountId, userId.Id.String()), nil
+}
