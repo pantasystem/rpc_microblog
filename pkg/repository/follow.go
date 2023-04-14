daeea861514b8850e31f88f9b8a564134a24a301
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-14 13:07:34 +0900

    feat: repositoryを作成

--- pkg/repository/follow.go
+++ pkg/repository/follow.go
@@ -1,1 +1,16 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"systems.panta/rpc-microblog/pkg/entity"
+)
+
+type FollowRepository interface {
+	Create(context.Context, *entity.Follow) (*entity.Follow, error)
+	Delete(context.Context, *entity.Follow) error
+	FindByFollowerId(context.Context, uuid.UUID) (*entity.Follow, error)
+	FindByAccountId(context.Context, uuid.UUID) (*entity.Follow, error)
+	FindById(context.Context, uuid.UUID) (*entity.Follow, error)
+}
