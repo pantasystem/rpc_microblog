a3c2b46e52fa9c6ab67dc85c036d3e606009f451
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-14 12:26:48 +0900

    feat: impl

--- pkg/dao/account.go
+++ pkg/dao/account.go
@@ -12,18 +12,36 @@ type AccountRepositoryImpl struct {
 	Db *gorm.DB
 }
 
-func (r *AccountRepositoryImpl) Create(context.Context, *entity.Account) (*entity.Account, error) {
-	return nil, nil
+func (r *AccountRepositoryImpl) Create(ctx context.Context, account *entity.Account) (*entity.Account, error) {
+	result := r.Db.Create(account)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return r.FindByID(ctx, account.Id)
 }
 
-func (r *AccountRepositoryImpl) FindByID(context.Context, uuid.UUID) (*entity.Account, error) {
-	return nil, nil
+func (r *AccountRepositoryImpl) FindByID(ctx context.Context, id uuid.UUID) (*entity.Account, error) {
+	var account entity.Account
+	result := r.Db.First(&account, id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &account, nil
 }
 
-func (r *AccountRepositoryImpl) FindByToken(context.Context, string) (*entity.Account, error) {
-	return nil, nil
+func (r *AccountRepositoryImpl) FindByToken(ctx context.Context, token string) (*entity.Account, error) {
+	var account entity.Account
+	result := r.Db.First(&account, "token = ?", token)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &account, nil
 }
 
-func (r *AccountRepositoryImpl) Delete(context.Context, uuid.UUID) error {
+func (r *AccountRepositoryImpl) Delete(ctx context.Context, id uuid.UUID) error {
+	result := r.Db.Delete(&entity.Account{}, id)
+	if result.Error != nil {
+		return result.Error
+	}
 	return nil
 }
