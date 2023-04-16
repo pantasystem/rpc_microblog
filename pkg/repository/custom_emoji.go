204503612a1d9b468ddda67eb866cf4bc46a87f1
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-16 21:15:10 +0900

    feat: custom emoji reopsitory

--- pkg/repository/custom_emoji.go
+++ pkg/repository/custom_emoji.go
@@ -1,1 +1,14 @@
 package repository
+
+import (
+	"context"
+
+	"systems.panta/rpc-microblog/pkg/entity"
+)
+
+type CustomEmojiRepository interface {
+	FindAll() (context.Context, []*entity.CustomEmoji, error)
+	Create(context.Context, *entity.CustomEmoji) (*entity.CustomEmoji, error)
+	Delete(context.Context, *entity.CustomEmoji) error
+	FindByName(context.Context, string) (*entity.CustomEmoji, error)
+}
