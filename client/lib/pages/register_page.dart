2ff1be1e12976f9545a763d73fec5ccdb7c6082b
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-24 16:19:13 +0900

    feat: 登録画面を作成

--- client/lib/pages/register_page.dart
+++ client/lib/pages/register_page.dart
@@ -1,6 +1,8 @@
 
+import 'package:client/state/auth_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class RegisterPage extends ConsumerStatefulWidget {
   const RegisterPage({super.key});
@@ -10,20 +12,62 @@ class RegisterPage extends ConsumerStatefulWidget {
 }
 
 class RegisterScreenState extends ConsumerState<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  final inputNameController = TextEditingController();
+  final inputAvatarUrlController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: const Text("Register"),
+        title: const Text("登録"),
       ),
-      body: Center(
+      body: Form(
+        key: _formKey,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("Register"),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: TextFormField(
+                    controller: inputNameController,
+                    decoration: const InputDecoration(label: Text("名前")),
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: TextFormField(
+                    controller: inputAvatarUrlController,
+                    decoration: const InputDecoration(
+                      label: Text("アバター画像URL"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  ref
+                      .read(authStoreProvider)
+                      .register(
+                    name: inputNameController.text,
+                    avatarUrl: inputAvatarUrlController.text,
+                  )
+                      .catchError((e, st) {
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(const SnackBar(content: Text("登録に失敗")));
+                  }).then((value) {
+                    context.go("/home");
+                  });
+                },
+                child: const Text("次へ")),
           ],
         ),
-      ),
+      )
     );
   }
 }
