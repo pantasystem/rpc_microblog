46d12cfed7e3bc87b695d0404c1eae0dbc9eca06
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-24 12:34:00 +0900

    feat: タイムラインのレスポンスを返せるように

---
+++ pkg/handler/timeline.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	"systems.panta/rpc-microblog/pkg/handler/proto"
+	"systems.panta/rpc-microblog/pkg/module"
+	"systems.panta/rpc-microblog/pkg/repository"
+)
+
+type TimelienService struct {
+	Module module.Module
+}
+
+func (r *TimelienService) GetTimeline(ctx context.Context, req *proto.TimelineRequest) (*proto.TimelineResponse, error) {
+	aId, ok := ctx.Value(AccountId).(string)
+	if !ok {
+		return nil, fmt.Errorf("account id not found")
+	}
+	aUuid, err := uuid.Parse(aId)
+	if err != nil {
+		return nil, err
+	}
+	q := &repository.FindByFollowedAccountQuery{}
+	if req.MaxId != nil {
+		maxId, err := uuid.Parse(*req.MaxId)
+		if err == nil {
+			q.MaxId = &maxId
+		}
+	}
+	if req.MinId != nil {
+		minId, err := uuid.Parse(*req.MinId)
+		if err == nil {
+			q.MinId = &minId
+		}
+	}
+	res, err := r.Module.RepositoryModule().StatusRepository().FindByFollowedAccount(ctx, aUuid, q)
+	if err != nil {
+		return nil, err
+	}
+	protoStatuses := make([]*proto.Status, len(res))
+	for i, s := range res {
+		protoStatuses[i] = ConvertToProtoModel(s, &aUuid)
+	}
+	return &proto.TimelineResponse{
+		Statuses: protoStatuses,
+	}, nil
+}
