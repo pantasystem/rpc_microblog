c361cb6d784a789b9aac96254079c3785ea27b61
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-26 19:25:01 +0900

    feat: providerに登録

--- client/lib/providers/repositories.dart
+++ client/lib/providers/repositories.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../repositories/account_repository.dart';
 import '../repositories/auth_repository.dart';
+import '../repositories/status_repository.dart';
 import 'grpc.dart';
 
 final authRepositoryProvider = Provider((ref) {
@@ -12,6 +13,13 @@ final authRepositoryProvider = Provider((ref) {
 final accountRepositoryProvider = Provider((ref) {
   return AccountRepository(
     client: ref.read(accountClientProvider),
+    authRepository: ref.read(authRepositoryProvider),
+  );
+});
+
+final statusRepositoryProvider = Provider((ref) {
+  return StatusRepository(
+    client: ref.read(statusClientProvider),
     authRepository: ref.read(authRepositoryProvider),
   );
 });
