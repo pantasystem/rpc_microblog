f1d7241cbbf1fdfaedd049033fefff2e91713a10
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-04-27 21:03:47 +0900

    feat: 下メニューを追加

--- client/lib/pages/home_page.dart
+++ client/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
-
+import 'package:client/pages/account_page.dart';
+import 'package:client/pages/search_user_page.dart';
 import 'package:client/pages/timeline_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,11 +13,12 @@ class HomePage extends ConsumerStatefulWidget {
 }
 
 class HomeScreenState extends ConsumerState {
+  HomeTab _currentTab = HomeTab.timeline;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
+        title: HomeAppBar(currentTab: _currentTab),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -24,7 +26,70 @@ class HomeScreenState extends ConsumerState {
         },
         child: const Icon(Icons.add),
       ),
-      body: const TimelinePage()
+      body: IndexedStack(
+        index: _currentTab.index,
+        children: const [
+          TimelinePage(),
+          SearchUserPage(),
+          AccountPage()
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentTab.index,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey,
+        unselectedFontSize: 10,
+        selectedFontSize: 10,
+        iconSize: 26,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        backgroundColor: Colors.white,
+        onTap: (int index) {
+          setState(() {
+            _currentTab = HomeTab.values[index];
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time_outlined),
+            label: "タイムライン",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: "Profile",
+          )
+        ],
+      ),
     );
   }
+}
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({super.key, required this.currentTab});
+
+  final HomeTab currentTab;
+
+
+  @override
+  Widget build(BuildContext context) {
+    switch(currentTab) {
+
+      case HomeTab.timeline:
+        return const Text("タイムライン");
+      case HomeTab.search:
+        return const Text("検索");
+      case HomeTab.profile:
+        return const Text("プロフィール");
+    }
+  }
+}
+
+enum HomeTab {
+  timeline,
+  search,
+  profile,
 }
---
+++ client/lib/pages/search_user_page.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class SearchUserPage extends StatelessWidget {
+  const SearchUserPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text(''),
+    );
+  }
+}
