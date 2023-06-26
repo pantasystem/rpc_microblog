a9d1886faff448036dd1a8b73ec247b19be64788
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-06-26 14:12:16 +0900

    fix

--- client/lib/pages/status_editor_page.dart
+++ client/lib/pages/status_editor_page.dart
@@ -2,7 +2,6 @@
 import 'dart:developer';
 
 import 'package:client/providers/repositories.dart';
-import 'package:client/state/timeline_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
